4e445cd052165903f71fcf4445750b834c416338
mikhail <michaelglukhoff@gmail.com>
2024-11-17 21:23:59 +0100

    Сохранены изменения в bot.py перед переключением ветки

--- bot.py
+++ bot.py
@@ -9,30 +9,30 @@ from logger import logging
 
 load_dotenv()
 
-token = os.getenv('TELEGRAM_BOT_TOKEN')
+token = os.getenv("TELEGRAM_BOT_TOKEN")
 
 if not token:
-    logging.error('Токен не установлен в файле .env.')
-    sys.exit("Завершение выполнения: переменные окружения не установлены.")
+	logging.error("No valid token in .env")
+	sys.exit("End process: environment variable not found")
 
 
 def main(bot):
-    register_handlers(bot)
+	register_handlers(bot)
 
-    while True:
-        try:
-            logging.info(f"Запущен bot.polling: {bot}")
-            bot.polling(non_stop=True, interval=0, timeout=20)
-        except Exception as e:
-            logging.error(f"Ошибка: {e}")
-            if isinstance(e, telebot.apihelper.ApiException) and e.error_code == 403:
-                logging.error("Бот был заблокирован пользователем.")
-            logging.info("Перезапуск бота через 5 секунд...")
-            logging.exception("Трассировка стека исключения:")
-            time.sleep(5)
+	while True:
+		try:
+			logging.info(f"Start bot.polling: {bot}")
+			bot.polling(non_stop=True, interval=0, timeout=20)
+		except Exception as e:
+			logging.error(f"Error: {e}")
+			if isinstance(e, telebot.apihelper.ApiException) and e.error_code == 403:
+				logging.error("Bot was blocked by user")
+			logging.info("Restarting bot in 5 seconds...")
+			logging.exception("Stack trace of the exception")
+			time.sleep(5)
 
 
 if __name__ == '__main__':
-    bot = telebot.TeleBot(token)
-    logging.info(f'Создан экземпляр бота: {bot}')
-    main(bot)
+	bot = telebot.TeleBot(token)
+	logging.info(f"Bot is created: {bot}")
+	main(bot)
